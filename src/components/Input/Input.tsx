1bd7f52ee1fe863e0355ec0ee3f46a2419a21cfd
rafaelteicas <rafaelteicas@gmail.com>
2023-10-20 17:48:12 -0300

    refactor: add fontFamily and function to get the right one

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextInputProps, TextStyle} from 'react-native';
 
 import {Pressable, TextInput} from 'react-native';
 
 import {Box, BoxProps, Text} from '@components';
 
+import {Icon} from '../Icon/Icon';
+
 interface inputProps extends TextInputProps {
   title?: string;
   placeholder?: string;
+  secureTextEntry?: boolean;
 }
 
-export function Input({title, placeholder, ...textInputProps}: inputProps) {
+export function Input({
+  title,
+  placeholder,
+  secureTextEntry,
+  ...textInputProps
+}: inputProps) {
+  const [secure, setSecure] = useState<boolean>();
+
   return (
     <Box {...$inputContainer}>
       <Pressable>
-        <Text color="primary">{title}</Text>
-        <Box>
-          <TextInput
-            placeholder={placeholder}
-            placeholderTextColor="white"
-            style={$inputTextStyle}
-            {...textInputProps}
-          />
+        <Box padding="s16">
+          <Text preset="paragraphMedium" bold color="primary">
+            {title}
+          </Text>
+          <Box flexDirection="row">
+            <TextInput
+              {...textInputProps}
+              secureTextEntry={secure}
+              style={[$inputTextStyle]}
+              placeholder={placeholder}
+              placeholderTextColor={'white'}
+            />
+            {secureTextEntry && (
+              <Icon
+                name={secure ? 'eyeIcon' : 'eyeOffIcon'}
+                onPress={() => setSecure(current => !current)}
+              />
+            )}
+          </Box>
         </Box>
       </Pressable>
     </Box>
@@ -29,12 +50,16 @@ export function Input({title, placeholder, ...textInputProps}: inputProps) {
 }
 
 const $inputTextStyle: TextStyle = {
+  padding: 0,
   flexGrow: 1,
   flexShrink: 1,
+  color: 'white',
+  fontSize: 12,
 };
 
 const $inputContainer: BoxProps = {
-  flexDirection: 'row',
   borderColor: 'primary',
   borderWidth: 1,
+  marginBottom: 's32',
+  borderRadius: 'b4',
 };
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {ComponentProps} from 'react';
 
+import {TextStyle} from 'react-native';
+
 import {createText} from '@shopify/restyle';
 
 import {Theme} from '@theme';
@@ -11,9 +13,55 @@ const RestyleText = createText<Theme>();
 type RestyleProps = ComponentProps<typeof RestyleText>;
 
 interface TextProps extends RestyleProps {
-  children: React.ReactNode;
+  preset: fontPresets;
+  children?: React.ReactNode;
+  bold?: boolean;
+  semiBold?: boolean;
 }
 
-export function Text({children, ...textProps}: TextProps) {
-  return <RestyleText {...textProps}>{children}</RestyleText>;
+export const $fontFamily = {
+  bold: 'Oswald-Bold',
+  semiBold: 'Oswald-SemiBold',
+  medium: 'Oswald-Medium',
+  regular: 'Oswald-Regular',
+  light: 'Oswald-Light',
+  extraLight: 'Oswald-ExtraLight',
+};
+
+type fontPresets = 'heading' | 'title' | 'paragraphMedium' | 'paragraphSmall';
+
+const $fontSize: Record<fontPresets, TextStyle> = {
+  heading: {fontSize: 20},
+  title: {fontSize: 28},
+  paragraphMedium: {fontSize: 16},
+  paragraphSmall: {fontSize: 12},
+};
+
+function getFontFamily(bold?: boolean, semiBold?: boolean) {
+  switch (true) {
+    case bold:
+      return $fontFamily.bold;
+    case semiBold:
+      return $fontFamily.semiBold;
+    default:
+      return $fontFamily.regular;
+  }
+}
+
+export function Text({
+  children,
+  bold,
+  semiBold,
+  preset,
+  ...textProps
+}: TextProps) {
+  const fontFamily = getFontFamily(bold, semiBold);
+  return (
+    <RestyleText
+      style={$fontSize[preset]}
+      fontFamily={fontFamily}
+      {...textProps}>
+      {children}
+    </RestyleText>
+  );
 }
