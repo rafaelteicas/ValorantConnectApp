868b97818c33afef1f84ee48b1c8078f6b6d513e
rafaelteicas <rafaelteicas@gmail.com>
2023-10-20 14:01:50 -0300

    feat: add fonts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -10,8 +10,21 @@ export const theme = createTheme({
     primary: pallette.red,
     backgroundColor: pallette.darkBlue,
   },
-  spacing: {},
-  borderRadii: {},
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s12: 12,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s28: 28,
+    s32: 32,
+  },
+  borderRadii: {
+    b4: 4,
+    b8: 8,
+    b12: 12,
+  },
   textVariants: {
     defaults: {},
   },
