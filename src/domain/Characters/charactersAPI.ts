50ad2e7e23b819298b441d1a345f659388180ee7
rafaelteicas <rafaelteicas@gmail.com>
2023-11-04 12:45:25 -0300

    refactor: add param isPlayableCharacter to remove duplicated data

--- src/domain/Characters/charactersAPI.ts
+++ src/domain/Characters/charactersAPI.ts
@@ -3,7 +3,9 @@ import {valorantAPIConfig} from '@api';
 import {CharactersTypeAPI} from './charactersType';
 
 async function getCharacters(): Promise<CharactersTypeAPI[]> {
-  const response = await valorantAPIConfig.get('agents');
+  const response = await valorantAPIConfig.get('agents', {
+    params: {isPlayableCharacter: true},
+  });
   return response.data.data;
 }
 
