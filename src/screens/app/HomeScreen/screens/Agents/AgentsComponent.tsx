892a700abd95cfaacf6ca889451603185e011528
rafaelteicas <rafaelteicas@gmail.com>
2023-11-04 12:45:51 -0300

    refactor: add background gradient

--- src/screens/app/HomeScreen/screens/Agents/AgentsComponent.tsx
+++ src/screens/app/HomeScreen/screens/Agents/AgentsComponent.tsx
@@ -4,6 +4,7 @@ import {Dimensions, ListRenderItemInfo} from 'react-native';
 
 import {Image} from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
 import {CharactersType} from 'src/domain/Characters/charactersType';
 
 import {Box} from '../../../../../components/Box/Box';
@@ -16,13 +17,11 @@ export function AgentsComponent({item}: ListRenderItemInfo<CharactersType>) {
   const DEFAULT_HEIGHT = height * 0.3;
 
   return (
-    <Box flex={1} alignItems="center" justifyContent="center">
-      <Image
-        source={{uri: item.background}}
-        width={DEFAULT_WIDTH}
-        height={DEFAULT_HEIGHT}
-        style={{position: 'absolute', opacity: 0.2}}
-      />
+    <Box
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      style={{backgroundColor: `#${item.backgroundGradientColors}`}}>
       <Image
         source={{uri: item.image}}
         width={DEFAULT_WIDTH}
@@ -31,6 +30,29 @@ export function AgentsComponent({item}: ListRenderItemInfo<CharactersType>) {
       <Text preset="heading" color="primary">
         {item.name}
       </Text>
+      <LinearGradient
+        colors={[
+          `#${item.backgroundGradientColors[0]}`,
+          `#${item.backgroundGradientColors[1]}`,
+        ]}
+        style={{
+          width: 140,
+          height: 180,
+          top: 80,
+          borderRadius: 10,
+          position: 'absolute',
+          zIndex: -1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+        }}>
+        <Image
+          source={{uri: item.background}}
+          width={DEFAULT_WIDTH}
+          height={DEFAULT_HEIGHT}
+          style={{opacity: 0.4, flex: 1}}
+        />
+      </LinearGradient>
     </Box>
   );
 }
