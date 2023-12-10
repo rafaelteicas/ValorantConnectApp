7b89372d0318ef7b20ce85ea5970d6a69fd73996
Rafael <rafaelteicas@gmail.com>
2023-12-10 18:02:09 -0300

    refactor: add props to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,19 +13,26 @@ export interface ButtonProps extends BoxProps {
   onPress?: (event: GestureResponderEvent) => void;
   outline?: boolean;
   preset?: Presets;
+  paddingHorizontalOn?: boolean;
 }
 
 export function Button({
   title,
   rightComponent,
-  onPress,
   disabled,
   preset = 'primary',
+  paddingHorizontalOn,
+  onPress,
   ...boxProps
 }: ButtonProps) {
   const buttonPreset = getButtonPresets[preset];
   return (
-    <TouchableOpacity disabled={disabled} onPress={onPress}>
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={{
+        paddingHorizontal: paddingHorizontalOn ? 32 : undefined,
+      }}>
       <Box
         {...$buttonStyle}
         justifyContent={rightComponent ? 'space-between' : 'center'}
